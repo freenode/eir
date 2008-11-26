5f7b9b2d9c54a9f2a2481c598f64314922dc7998
Stephen Bennett <spb@exherbo.org>
2008-11-26 18:57:33 +0000

    Make Bot use PrivateImplementationPattern.

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,13 +1,49 @@
 #include "bot.h"
 #include "command.h"
 
+#include <paludis/util/wrapped_forward_iterator-impl.hh>
+#include <paludis/util/private_implementation_pattern-impl.hh>
+
 using namespace eir;
 
 using namespace std::tr1::placeholders;
+using paludis::Implementation;
+
+namespace paludis
+{
+    template <>
+    struct Implementation<Bot>
+    {
+        typedef std::map<std::string, Client::ptr> ClientMap;
+        typedef std::map<std::string, Channel::ptr> ChannelMap;
+        typedef std::map<std::string, std::string> SettingsMap;
+
+        Bot *bot;
+
+        Server _server;
+        std::string _host, _port, _nick, _pass;
+
+        Client::ptr _me;
+
+        ClientMap _clients;
+        ChannelMap _channels;
+        SettingsMap _settings;
+
+        void handle_message(std::string);
+
+        void _init_me(const Message *);
+
+        Implementation(Bot *b, std::string host, std::string port, std::string nick, std::string pass)
+            : bot(b), _server(std::tr1::bind(&Implementation<Bot>::handle_message, this, _1)),
+              _host(host), _port(port), _nick(nick), _pass(pass)
+        {
+        }
+    };
+}
+
 
 Bot::Bot(std::string host, std::string port, std::string nick, std::string pass)
-    : _server(std::tr1::bind(&Bot::handle_message, this, _1)),
-      _host(host), _port(port), _nick(nick), _pass(pass)
+    : paludis::PrivateImplementationPattern<Bot>(new paludis::Implementation<Bot>(this, host, port, nick, pass))
 {
 }
 
@@ -15,9 +51,9 @@ Bot::~Bot()
 {
 }
 
-void Bot::handle_message(std::string line)
+void paludis::Implementation<Bot>::handle_message(std::string line)
 {
-    Message m(this);
+    Message m(bot);
     std::string::size_type p1, p2;
 
     std::string::iterator e = line.end();
@@ -48,7 +84,7 @@ void Bot::handle_message(std::string line)
     if (bang != std::string::npos)
     {
         std::string nick = m.source.raw.substr(0, bang);
-        ClientIterator c = find_client(nick);
+        Bot::ClientIterator c = bot->find_client(nick);
         if (c != _clients.end())
             m.source.client = c->second;
 
@@ -95,34 +131,124 @@ void Bot::handle_message(std::string line)
     CommandRegistry::get_instance()->dispatch(&m);
 }
 
-void Bot::_init_me(const Message *m)
+void paludis::Implementation<Bot>::_init_me(const Message *m)
 {
     _nick = m->source.destination;
     Client::ptr c(new Client(_nick, "", ""));
     _me = c;
     _clients.insert(make_pair(_nick, _me));
-    send("USERHOST " + _nick);
+    bot->send("USERHOST " + _nick);
 }
 
 
 void Bot::run()
 {
-    _server.connect(_host, _port);
+    _imp->_server.connect(_imp->_host, _imp->_port);
 
     Message m(this, "on_connect");
     CommandRegistry::get_instance()->dispatch(&m);
 
-    if (_pass.length() > 0)
-        _server.send("PASS " + _pass);
+    if (_imp->_pass.length() > 0)
+        _imp->_server.send("PASS " + _imp->_pass);
 
-    _server.send("NICK " + _nick);
-    _server.send("USER eir * * :eir version 0.0.1");
+    _imp->_server.send("NICK " + _imp->_nick);
+    _imp->_server.send("USER eir * * :eir version 0.0.1");
 
-    _server.run();
+    _imp->_server.run();
 }
 
 void Bot::send(std::string line)
 {
-    _server.send(line);
+    _imp->_server.send(line);
 }
 
+// Client stuff
+
+Bot::ClientIterator Bot::begin_clients()
+{
+    return _imp->_clients.begin();
+}
+
+Bot::ClientIterator Bot::end_clients()
+{
+    return _imp->_clients.end();
+}
+
+Bot::ClientIterator Bot::find_client(std::string nick)
+{
+    return _imp->_clients.find(nick);
+}
+
+std::pair<Bot::ClientIterator, bool> Bot::add_client(Client::ptr c)
+{
+    return _imp->_clients.insert(make_pair(c->nick(), c));
+}
+
+unsigned long Bot::remove_client(Client::ptr c)
+{
+    return _imp->_clients.erase(c->nick());
+}
+
+// Channel stuff
+
+Bot::ChannelIterator Bot::begin_channels()
+{
+    return _imp->_channels.begin();
+}
+
+Bot::ChannelIterator Bot::end_channels()
+{
+    return _imp->_channels.end();
+}
+
+Bot::ChannelIterator Bot::find_channel(std::string name)
+{
+    return _imp->_channels.find(name);
+}
+
+std::pair<Bot::ChannelIterator, bool> Bot::add_channel(Channel::ptr c)
+{
+    return _imp->_channels.insert(make_pair(c->name(), c));
+}
+
+unsigned long Bot::remove_channel(Channel::ptr c)
+{
+    return _imp->_channels.erase(c->name());
+}
+
+void Bot::remove_channel(Bot::ChannelIterator c)
+{
+    _imp->_channels.erase(c.underlying_iterator<Implementation<Bot>::ChannelMap::iterator>());
+}
+
+// Settings stuff
+
+Bot::SettingsIterator Bot::begin_settings()
+{
+    return _imp->_settings.begin();
+}
+
+Bot::SettingsIterator Bot::end_settings()
+{
+    return _imp->_settings.end();
+}
+
+Bot::SettingsIterator Bot::find_setting(std::string name)
+{
+    return _imp->_settings.find(name);
+}
+
+std::pair<Bot::SettingsIterator, bool> Bot::add_setting(std::string n, std::string s)
+{
+    return _imp->_settings.insert(make_pair(n, s));
+}
+
+unsigned long Bot::remove_setting(std::string n)
+{
+    return _imp->_settings.erase(n);
+}
+
+void Bot::remove_setting(Bot::SettingsIterator it)
+{
+    _imp->_settings.erase(it.underlying_iterator<Implementation<Bot>::SettingsMap::iterator>());
+}
--- src/bot.h
+++ src/bot.h
@@ -2,95 +2,61 @@
 #define bot_h
 
 #include <string>
-#include <map>
 
 #include <paludis/util/wrapped_forward_iterator.hh>
+#include <paludis/util/private_implementation_pattern.hh>
 
 #include "server.h"
 #include "client.h"
 #include "message.h"
 
-namespace eir {
-
-    class Bot
+namespace eir
+{
+    class Bot : public paludis::PrivateImplementationPattern<Bot>
     {
         public:
             Bot(std::string host, std::string port, std::string nick, std::string pass);
 
-            const std::string& nick() { return _nick; }
+            const std::string& nick();
 
-            const Client::ptr me() { return _me; }
+            const Client::ptr me();
 
             void run();
 
             void send(std::string);
 
-            typedef std::map<std::string, Client::ptr> ClientMap;
-            typedef ClientMap::iterator ClientIterator;
-            ClientIterator begin_clients() { return _clients.begin(); }
-            ClientIterator end_clients() { return _clients.end(); }
-            ClientIterator find_client(std::string nick) {
-                return _clients.find(nick);
-            }
-            std::pair<ClientIterator, bool> add_client(Client::ptr c) {
-                return _clients.insert(make_pair(c->nick(), c));
-            }
-            ClientMap::size_type remove_client(Client::ptr c) {
-                return _clients.erase(c->nick());
-            }
+            struct ClientIteratorTag;
+            typedef paludis::WrappedForwardIterator<ClientIteratorTag,
+                                        const std::pair<const std::string, Client::ptr> > ClientIterator;
+            ClientIterator begin_clients();
+            ClientIterator end_clients();
+            ClientIterator find_client(std::string nick);
+            std::pair<ClientIterator, bool> add_client(Client::ptr c);
+            unsigned long remove_client(Client::ptr c);
 
-            typedef std::map<std::string, Channel::ptr> ChannelMap;
-            typedef ChannelMap::iterator ChannelIterator;
-            ChannelIterator begin_channels() { return _channels.begin(); }
-            ChannelIterator end_channels() { return _channels.end(); }
-            ChannelIterator find_channel(std::string name) {
-                return _channels.find(name);
-            }
-            std::pair<ChannelIterator, bool> add_channel(Channel::ptr c) {
-                return _channels.insert(make_pair(c->name(), c));
-            }
-            ChannelMap::size_type remove_channel(Channel::ptr c) {
-                return _channels.erase(c->name());
-            }
-            void remove_channel(ChannelIterator c) {
-                _channels.erase(c);
-            }
+            struct ChannelIteratorTag;
+            typedef paludis::WrappedForwardIterator<ChannelIteratorTag,
+                                        const std::pair<const std::string, Channel::ptr> > ChannelIterator;
+            ChannelIterator begin_channels();
+            ChannelIterator end_channels();
+            ChannelIterator find_channel(std::string name);
+            std::pair<ChannelIterator, bool> add_channel(Channel::ptr c);
+            unsigned long remove_channel(Channel::ptr c);
+            void remove_channel(ChannelIterator c);
 
-            typedef std::map<std::string, std::string> SettingsMap;
-            typedef SettingsMap::iterator SettingsIterator;
-            SettingsIterator begin_settings() { return _settings.begin(); }
-            SettingsIterator end_settings() { return _settings.end(); }
-            SettingsIterator find_setting(std::string name) {
-                return _settings.find(name);
-            }
-            std::pair<SettingsIterator, bool> add_setting(std::string n, std::string s) {
-                return _settings.insert(make_pair(n, s));
-            }
-            SettingsMap::size_type remove_setting(std::string n) {
-                return _settings.erase(n);
-            }
-            void remove_setting(SettingsIterator it) {
-                _settings.erase(it);
-            }
+            struct SettingsIteratorTag;
+            typedef paludis::WrappedForwardIterator<SettingsIteratorTag,
+                                        const std::pair<const std::string, std::string> > SettingsIterator;
+            SettingsIterator begin_settings();
+            SettingsIterator end_settings();
+            SettingsIterator find_setting(std::string name);
+            std::pair<SettingsIterator, bool> add_setting(std::string n, std::string s);
+            unsigned long remove_setting(std::string n);
+            void remove_setting(SettingsIterator it);
 
             Bot();
             ~Bot();
-
-        private:
-            Server _server;
-            std::string _host, _port, _nick, _pass;
-
-            Client::ptr _me;
-
-            ClientMap _clients;
-            ChannelMap _channels;
-            SettingsMap _settings;
-
-            void handle_message(std::string);
-
-            void _init_me(const Message *);
-    };
-
+   };
 }
 
 #endif
