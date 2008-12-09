3723ebce3a17ef50d4cd89985750bbfba78c237f
Stephen Bennett <spb@exherbo.org>
2008-12-09 19:47:56 +0000

    Add a little utility thing to simplify registering message handlers

---
+++ src/handler.h
@@ -0,0 +1,24 @@
+#include "command.h"
+#include <functional>
+
+
+namespace eir
+{
+    template <class T_>
+    struct CommandHandlerBase
+    {
+        template<class F_>
+        CommandRegistry::id add_handler(std::string s, F_ f)
+        {
+            return eir::CommandRegistry::get_instance()->add_handler(s,
+                    std::tr1::bind(f, static_cast<T_*>(this), std::tr1::placeholders::_1));
+        }
+
+        void remove_handler(CommandRegistry::id i)
+        {
+            eir::CommandRegistry::get_instance()->remove_handler(i);
+        }
+    };
+}
+
+
