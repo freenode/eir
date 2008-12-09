e80c91717a258b8c009700a9137e47c97be050fe
Stephen Bennett <spb@exherbo.org>
2008-12-09 19:48:30 +0000

    Add message hander to load modules. Currently only accepts input from the config file, which doesn't exist yet.

---
+++ src/modload.cpp
@@ -0,0 +1,82 @@
+#include "eir.h"
+
+#include "handler.h"
+
+using namespace std::tr1::placeholders;
+using namespace eir;
+
+struct Modloader : public CommandHandlerBase<Modloader>
+{
+    eir::CommandRegistry::id modload_id, modunload_id, modreload_id;
+
+    void do_modload(const eir::Message *m)
+    {
+        if(m->source.special != sourceinfo::ConfigFile)
+            return;
+
+        try
+        {
+            ModuleRegistry::get_instance()->load(m->args[0]);
+        }
+        catch(ModuleError &e)
+        {
+            m->source.reply("Failed to load " + m->args[0] + ": " + e.what());
+        }
+    }
+
+    void do_modunload(const eir::Message *m)
+    {
+        if(m->source.special != sourceinfo::ConfigFile)
+            return;
+
+        try
+        {
+            ModuleRegistry::get_instance()->unload(m->args[0]);
+        }
+        catch(ModuleError &e)
+        {
+            m->source.reply("Failed to unload " + m->args[0] + ": " + e.what());
+        }
+     }
+
+    void do_modreload(const eir::Message *m)
+    {
+        if(m->source.special != sourceinfo::ConfigFile)
+            return;
+
+        if (ModuleRegistry::get_instance()->is_loaded(m->args[0]))
+        {
+            try
+            {
+                ModuleRegistry::get_instance()->load(m->args[0]);
+            }
+            catch(ModuleError &e)
+            {
+                m->source.reply("Failed to unload " + m->args[0] + ": " + e.what());
+                return;
+            }
+        }
+        try
+        {
+            ModuleRegistry::get_instance()->load(m->args[0]);
+        }
+        catch(ModuleError &e)
+        {
+            m->source.reply("Failed to load " + m->args[0] + ": " + e.what());
+        }
+     }
+
+    Modloader() {
+        modload_id = add_handler("modload", &Modloader::do_modload);
+        modunload_id = add_handler("modunload", &Modloader::do_modunload);
+        modreload_id = add_handler("modreload", &Modloader::do_modreload);
+    }
+
+    ~Modloader() {
+        remove_handler(modload_id);
+        remove_handler(modunload_id);
+        remove_handler(modreload_id);
+    }
+};
+
+Modloader m;
