c12e0cb288df49b2119dbcc8b78b59796a798a3b
Stephen Bennett <spb@exherbo.org>
2009-09-24 21:24:11 +0100

    Add dummy function to force type info for all exception classes to be generated in the main executable

---
+++ src/exceptions.cpp
@@ -0,0 +1,24 @@
+// This file shouldn't really exist.
+// Unfortunately, type info for all the exception classes has to
+// exist inside the main executable, or throwing an exception from
+// one module that's caught in another will fail.
+
+#include "exceptions.h"
+
+using namespace eir;
+
+namespace {
+    void silly_dummy_function()
+    {
+        DieException a("");
+        RestartException b;
+        NotConnectedException c;
+        ConnectionError d("");
+        ModuleError e("");
+        ConfigurationError f("");
+        InternalError g("");
+        StorageError h("");
+        IOError i("");
+    }
+}
+
