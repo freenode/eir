72ca8a71a7cbebed1a69df951b4dbf015120ef25
Mike Quin <mike@elite.uk.com>
2011-08-16 17:27:20 +0100

    bantracker: Fix syntax error in sqlite schema

--- doc/bantracker/schema-sqlite3.sql
+++ doc/bantracker/schema-sqlite3.sql
@@ -1,2 +1,2 @@
-CREATE TABLE bans ( i INTEGER PRIMARY KEY, channel VARCHAR, setter VARCHAR, mask VARCHAR, isSet BOOL, setDate DATETIME, reason VARCHAR, action INTEGER, actionDate DATETIME, "affected" TEXT, unbanner TEXT, unbanDate DATETIME, type varchar, nagged integer)
+CREATE TABLE bans ( i INTEGER PRIMARY KEY, channel VARCHAR, setter VARCHAR, mask VARCHAR, isSet BOOL, setDate DATETIME, reason VARCHAR, action INTEGER, actionDate DATETIME, "affected" TEXT, unbanner TEXT, unbanDate DATETIME, type varchar, nagged integer);
 CREATE TABLE log (i INTEGER PRIMARY KEY, channel VARCHAR, operator VARCHAR, mask VARCHAR, action VARCHAR, date DATETIME);
